d13a2060a588706a6a90b2bf251ed5fe5d17c8b1
ifmiss <185098535@qq.com>
2020-03-07 00:56:18 +0800

    update: server.js

--- server.js
+++ server.js
@@ -3,7 +3,6 @@ const next = require('next')
 const Router = require('koa-router')
 const dev = process.env.NODE_ENV !== 'production';
 const app = next({ dev });
-const handle = app.getRequestHandler();
 
 // const middleware = require('./middleware')
 
@@ -16,27 +15,25 @@ app.prepare().then(() => {
   try {
     router.get('/', async (ctx) => {
       await app.render(ctx.req, ctx.res, '/', ctx.query)
-      ctx.respond = false
     })
   
     router.get('/blog', async (ctx) => {
       await app.render(ctx.req, ctx.res, `/blog`)
-      ctx.respond = false
     })
   
     router.get('/blog/detail/:id', async (ctx) => {
       const { id } = ctx.params
       await app.render(ctx.req, ctx.res, `/blog/detail/${id}`, {id})
-      ctx.respond = false
     })
   } catch (e) {
     console.log(e)
   }
   // 如果没有配置nginx做静态文件服务，下面代码请务必开启
-  // router.get('*', async ctx => {
-  //   await handle(ctx.req, ctx.res)
-  //   ctx.respond = false
-  // })
+  router.get('*', async ctx => {
+    await app.render(ctx.req, ctx.res, '/blog', ctx.query)
+    ctx.status = 200;
+    ctx.respond = false;
+  })
 
   server.use(router.routes())
 
