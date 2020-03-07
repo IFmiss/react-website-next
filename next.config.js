c2b9d90fce45604dc5f2431d26386b6cb122037f
ifmiss <185098535@qq.com>
2020-03-08 01:06:16 +0800

    111

--- next.config.js
+++ next.config.js
@@ -13,21 +13,6 @@ if (typeof require !== 'undefined') {
   require.extensions['.less'] = (file) => {}
 }
 
-function HACK_removeMinimizeOptionFromCssLoaders(config) {
-  console.warn(
-    'HACK: Removing `minimize` option from `css-loader` entries in Webpack config',
-  );
-  config.module.rules.forEach(rule => {
-    if (Array.isArray(rule.use)) {
-      rule.use.forEach(u => {
-        if (u.loader === 'css-loader' && u.options) {
-          delete u.options.minimize;
-        }
-      });
-    }
-  });
-}
-
 module.exports = withPlugins(
   [
     withLess
