1dd898510a22f364db0b3b2646f9a3de79221dec
ifmiss <185098535@qq.com>
2020-03-08 13:51:15 +0800

    change: server.js

--- server.js
+++ server.js
@@ -4,7 +4,7 @@ const Router = require('koa-router')
 const dev = process.env.NODE_ENV !== 'production';
 const bodyParser = require('koa-bodyparser');
 const app = next({ dev });
-
+const handle = app.getRequestHandler()
 // const middleware = require('./middleware')
 
 app.prepare().then(() => {
@@ -15,25 +15,33 @@ app.prepare().then(() => {
 
   try {
     router.get('/', async (ctx) => {
-      await app.render(ctx.req, ctx.res, '/', ctx.query)
+      return app.render(ctx.req, ctx.res, '/', ctx.query)
     })
   
     router.get('/blog', async (ctx) => {
-      await app.render(ctx.req, ctx.res, `/blog`)
+      return app.render(ctx.req, ctx.res, `/blog`, ctx.query)
     })
   
+    // router.get('/blog/detail/:id', async (ctx) => {
+    //   return app.render(ctx.req, ctx.res, `/blog/detail`, {id: ctx.params.id})
+    // })
+
     router.get('/blog/detail', async (ctx) => {
-      console.log('ctx.detail id', ctx.query.id)
       await app.render(ctx.req, ctx.res, `/blog/detail`, {id: ctx.query.id})
     })
   } catch (e) {
     console.log('e-----e', e)
   }
   // 如果没有配置nginx做静态文件服务，下面代码请务必开启
-  router.get('*', async ctx => {
-    await app.render(ctx.req, ctx.res, '/', ctx.query)
-    ctx.status = 200;
-    ctx.respond = false;
+  // router.get('*', async ctx => {
+  //   await app.render(ctx.req, ctx.res, '/', ctx.query)
+  //   ctx.status = 200;
+  //   ctx.respond = false;
+  // })
+
+  server.use(async (ctx, next) => {
+    await handle(ctx.req, ctx.res)
+    ctx.respond = false
   })
 
   server.use(bodyParser())
