c2700be3ee949beab4abe0389a94024c0c27a618
ifmiss <185098535@qq.com>
2020-04-12 16:34:02 +0800

    update: pm2 conf

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -3,7 +3,7 @@ module.exports = {
     {
       name: 'web-next',
       script: 'npm',
-      args: 'start',
+      args: 'run start',
       instances: 'max',
       exec_mode: 'cluster',
       max_memory_restart: '200M',
