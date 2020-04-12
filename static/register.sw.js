7c049e722c62644c8c5fed54219a8f4912cff5c8
ifmiss <185098535@qq.com>
2020-04-12 17:09:13 +0800

    update: dist

--- static/register.sw.js
+++ static/register.sw.js
@@ -1,5 +1,3 @@
-(function() {
-  if ('serviceWorker' in navigator) {
-      navigator.serviceWorker.register('./service-worker.js');
-  }
-})()
+if ('serviceWorker' in navigator) {
+  navigator.serviceWorker.register('./service-worker.js');
+}
