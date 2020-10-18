8cb42f7716f6a5daa7783ab872a545a242cb3dcf
ifmiss <185098535@qq.com>
2020-10-18 17:15:51 +0800

    skeleton

---
+++ components/Skeleton/index.tsx
@@ -0,0 +1,22 @@
+import React, { useEffect } from 'react'
+import classNames from 'classnames'
+// import './nav.less'
+import {
+  PROJECT_NAME,
+  WEBSITE_TITLE
+} from '@constance/index' 
+
+export interface SkeletonProps {
+  routerName: 'home' | 'blogList' | 'blogDetail'
+}
+
+const Skeleton: React.FC<SkeletonProps> = ({
+  routerName
+}) => {
+  console.info(routerName);
+  return (
+    <div>123123</div>
+  );
+};
+
+export default Skeleton;
