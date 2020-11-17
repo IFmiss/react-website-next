e439e67e5d9db5f6e68b3397a9e0c4ab46a7faa7
ifmiss <185098535@qq.com>
2020-11-17 18:29:44 +0800

    update: ps

--- static/registerSw.js
+++ static/registerSw.js
@@ -1,4 +1,42 @@
-const webpush = require('web-push');
+// importScripts("https://www.gstatic.com/firebasejs/7.16.1/firebase-app.js");
+// importScripts(
+//     "https://www.gstatic.com/firebasejs/7.16.1/firebase-messaging.js",
+// );
+// // For an optimal experience using Cloud Messaging, also add the Firebase SDK for Analytics.
+// importScripts(
+//     "https://www.gstatic.com/firebasejs/7.16.1/firebase-analytics.js",
+// );
+
+// firebase.initializeApp({
+//   apiKey: "AIzaSyAMTZYWmwmqnl1a7jpdkc8gmiL1UNb39Ek",
+//   authDomain: "website-10286.firebaseapp.com",
+//   databaseURL: "https://website-10286.firebaseio.com",
+//   projectId: "website-10286",
+//   storageBucket: "website-10286.appspot.com",
+//   messagingSenderId: "960070276528",
+//   appId: "1:960070276528:web:f7fa9dc8d045f79846b275",
+//   measurementId: "G-1DMP961GLG"
+// });
+
+// const messaging = firebase.messaging();
+
+// messaging.setBackgroundMessageHandler(function(payload) {
+//   console.log(
+//       "[firebase-messaging-sw.js] Received background message ",
+//       payload,
+//   );
+//   // Customize notification here
+//   const notificationTitle = "Background Message Title";
+//   const notificationOptions = {
+//       body: "Background Message body.",
+//       icon: "/itwonders-web-logo.png",
+//   };
+
+//   return self.registration.showNotification(
+//       notificationTitle,
+//       notificationOptions,
+//   );
+// });
 
 if ('serviceWorker' in navigator) {
   navigator.serviceWorker.register('/sw.js').then((registration) => {
@@ -9,37 +47,29 @@ if ('serviceWorker' in navigator) {
         if (!subscription) {
           subscribeUser(registration);
         } else {
+          console.info(subscription)
           console.log("You have subscribed our notification");
         } 
       })
     }
-  }).then((registration) => {
-      // Notification.requestPermission()
-      // .then((status) => {
-      //   if (status === 'granted') {
-      //     registration?.showNotification('foo');
-      //     subscribeUser(registration);
-      //     console.info(registration)
-      //   }
-      // }).catch(e => {
-      //   if (Notification.permission === 'denied') {
-      //     console.log('Permission for Notifications was denied');
-      //   } else {
-      //     console.log('Unable to subscribe to push.', e);
-      //   }
-      // })
-  });
+  })
 }
 
 function subscribeUser(registration) {
-  const applicationServerPublicKey = webpush.generateVAPIDKeys();
+  // Public Key:
+  // BJOVnfkuVBZBXMVVzdFOtQGbAHvdoF7-7AcGHa7J8bGEL3tI_Htb7bxGtsNzTiHqmcXwm7YkNNO_0auf1AP8m-A
+
+  // Private Key:
+  // FRuBFzS-wqHTRBneR0YlupJz8i1pagf_KJSPTJI1Esg
+
+  const applicationServerPublicKey = 'BJOVnfkuVBZBXMVVzdFOtQGbAHvdoF7-7AcGHa7J8bGEL3tI_Htb7bxGtsNzTiHqmcXwm7YkNNO_0auf1AP8m-A';
   const applicationServerKey = urlBase64ToUint8Array(applicationServerPublicKey);
   registration.pushManager.subscribe({
       userVisibleOnly: true,
       applicationServerKey: applicationServerKey
   }).then(function(subscription) {
-    console.log('User is subscribed:', JSON.stringify(subscription));
-    console.info({subscription: JSON.stringify(subscription)})
+    // ajax
+
   }).catch(function(err) {
     // 用户不同意或者生成失败
     console.log('Failed to subscribe the user: ', err);
@@ -63,3 +93,8 @@ function urlBase64ToUint8Array(base64String) {
   }
   return outputArray;
 }
+
+function uint8ArrayToBase64 (arr) {
+  return btoa(String.fromCharCode.apply(null, new Uint8Array(arr)))
+}
+
